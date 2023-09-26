510dd451643ed116cadfeb22fa1e7ddc446996b6
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2023-09-26 01:27:31 +0100

    Create arraySort.js

---
+++ RandomExe_js/arraySort.js
@@ -0,0 +1,13 @@
+
+let arr = [55, 2, 9, 1, 9];
+
+for (let i = 0; i < arr.length - 1; i++) {
+  for (let j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let chh = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = chh;
+    }
+  }
+}
+console.log(arr);
